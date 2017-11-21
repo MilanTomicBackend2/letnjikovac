25df11e8dd1d8c3997ff05010052f711dd4170ee
MilanTomic0212 <mtomic62s@gmail.com>
2017-11-21 15:15:45 +0100

    80%

--- src/AppBundle/Entity/Event.php
+++ src/AppBundle/Entity/Event.php
@@ -3,10 +3,15 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * Event
  *
+ * @ORM\Entity
+ * @Vich\Uploadable
+ * 
  * @ORM\Table(name="event")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\EventRepository")
  */
@@ -20,6 +25,25 @@ class Event
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
+    
+    /**
+     * @Vich\UploadableField(mapping="event_image", fileNameProperty="image")
+     * @var File $imageFile
+     */
+    private $imageFile;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255)
+     */
+    private $image;
+    
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $updatedAt;
 
     /**
      * @var string
@@ -41,6 +65,37 @@ class Event
      * @ORM\Column(name="description", type="text")
      */
     private $description;
+    
+    /**
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+     */
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
 
 
     /**
